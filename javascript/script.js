0e609201b241a17cc2f6b3ff6608e05191a8d1c9
PokeP31 <pokep31@gmail.com>
2022-09-17 23:21:34 +0200

    Fin del proyecto

--- javascript/script.js
+++ javascript/script.js
@@ -1,51 +1,20 @@
-const input = document.querySelector("input"),
-      showHide = document.querySelector(".show_hide"),
-      indicator = document.querySelector(".indicator"),
-      iconText = document.querySelector(".icon-text"),
+// getting all attributes
+const form = document.querySelector("form"),
+      eInput = document.querySelector("input"),
       text = document.querySelector(".text");
 
-    // JS code to show & hide password  
-      showHide.addEventListener("click", () =>{
-        if(input.type === "password"){
-          input.type = "text";
-          showHide.classList.replace("fa-eye-slash", "fa-eye")
-        }else{
-          input.type = "password"
-          showHide.classList.replace("fa-eye", "fa-eye-slash")
-        }
-      })
+form.addEventListener("submit", (e) =>{
+  e.preventDefault(); // evita el comportamiento predeterminado
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; // RegEx pattern to validate emails
 
-    // JS code to show password strength (with regex)
-      let alphabet = /[a-zA-Z]/, //letter a to z & A to Z
-          numbers = /[0-9]/, //number 0 to 9
-          scharacters = /[!,@,#,$,%,^,&,*,?,_,(,),-,+,=,~]/; //special characters
-      input.addEventListener("keyup", () => {
-        indicator.classList.add("active");
-
-        let val = input.value;
-        if(val.match(alphabet) || val.match(numbers) || val.match(scharacters)){
-          text.textContent = "La clave es débil";
-          input.style.borderColor = "#ff6333";
-          iconText.style.color = "#ff6333";
-          showHide.style.color = "#ff6333";
-        }
-        if(val.match(alphabet) && val.match(numbers) && val.length >= 6){
-          text.textContent = "La clave es media";
-          input.style.borderColor = "#cc8500";
-          iconText.style.color = "#cc8500";
-          showHide.style.color = "#cc8500";
-        }
-        if(val.match(alphabet) && val.match(numbers) && val.match(scharacters) && val.length >= 8){
-          text.textContent = "La clave es fuerte";
-          input.style.borderColor = "#22c32a";
-          iconText.style.color = "#22c32a";
-          showHide.style.color = "#22c32a";
-        }
-        
-        if(val == ""){
-          indicator.classList.remove("active");
-          input.style.borderColor = "#a6a6a6";
-          iconText.style.color = "#a6a6a6";
-          showHide.style.color = "#a6a6a6";
-        }
-      });
+  form.classList.add("error")
+  form.classList.remove("valid")
+  if(eInput.value == ""){
+    text.innerText = "El email no puede estar en blanco";
+  }else if (!eInput.value.match(pattern)){ //si el patrón no coincide con el valor introducido
+    text.innerText = "Por favor, introduce un email correcto";
+  }else{
+    form.classList.replace("error", "valid");
+    text.innerText = "El email es correcto";
+  }
+})
